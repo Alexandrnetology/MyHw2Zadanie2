27a811d4c46050238c3b1f5c4bcaf6996c14283b
alexander test <martunov2013@mail.ru>
2024-02-23 12:30:57 +0300

    Added programm

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,15 @@ public class Main {
         // сумма пополнения порог, и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
         // на экран.
+        int NachSchet = 100;
+        int symm = 1100;
+        if (symm < 1000) {
+            int balans = symm + NachSchet;
+            System.out.println(balans);
+        }
+        if (symm >= 1000) {
+            int BalansBonus = symm / 100 + symm + NachSchet;
+            System.out.println(BalansBonus);
+        }
     }
 }
